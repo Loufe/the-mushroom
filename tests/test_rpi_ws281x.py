4e71c9914fe9736e6d699cb9e114705f5fdfdb0e
Lou Bernardi <loufeadmin@gmail.com>
2025-09-09 00:31:29 -0400

    change to gradient blend test

--- tests/test_rpi_ws281x.py
+++ tests/test_rpi_ws281x.py
@@ -11,30 +11,33 @@ LED_INVERT = False
 LED_CHANNEL = 0
 LED_STRIP = ws.WS2811_STRIP_GRB
 
+def wheel(pos):
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
+
 def main():
     strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
     strip.begin()
     
     print(f"Testing {LED_COUNT} WS2811 LEDs on GPIO {LED_PIN}")
-    print("Blue -> Purple -> Red (1 second each)")
+    print("Fast rainbow gradient - all LEDs same color")
     print("Press Ctrl-C to stop")
     
     try:
+        j = 0
         while True:
+            color = wheel(j & 255)
             for i in range(LED_COUNT):
-                strip.setPixelColor(i, Color(0, 0, 255))
+                strip.setPixelColor(i, color)
             strip.show()
-            time.sleep(1)
-            
-            for i in range(LED_COUNT):
-                strip.setPixelColor(i, Color(128, 0, 128))
-            strip.show()
-            time.sleep(1)
-            
-            for i in range(LED_COUNT):
-                strip.setPixelColor(i, Color(255, 0, 0))
-            strip.show()
-            time.sleep(1)
+            time.sleep(0.01)
+            j += 2
             
     except KeyboardInterrupt:
         for i in range(LED_COUNT):
